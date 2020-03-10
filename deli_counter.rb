716bc6f101a04b10d7ef2ec3be4e8edffc1f3c65
Brandon Marrero <brandonmar97@gmail.com>
2020-03-10 21:06:25 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,16 +2,8 @@
 
 katz_deli = []
 
-def line(numinline)
-  line_array = []
-  if numinline.length == 0
-    puts "The line is currently empty."
-  else
-    line_array.each.with_index(0) do |name, index|
-      line_array.push("#{index}. #{name}")
-    end
-    puts "The line is: #{line_array.join(" ")}"
-  end
+def line(katz_deli)
+  line_
 end
 
 def take_a_number(array, string)
