2d6c23c23ca2d77dbc56ce107590380a257ec274
Brandon Marrero <brandonmar97@gmail.com>
2020-03-10 21:09:52 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -12,7 +12,7 @@ def line(katz_deli)
     line_pos << "#{number}. #{customer}"
     number += 1
   end
-    puts "The line is currently: #{line_positions.join(" ")}"
+    puts "The line is currently: #{line_pos.join(" ")}"
   end
 end
 
