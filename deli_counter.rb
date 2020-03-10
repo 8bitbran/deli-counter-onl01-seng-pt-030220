834660f6f8df7210e12e8624021a900faa924ca1
Brandon Marrero <brandonmar97@gmail.com>
2020-03-10 21:02:35 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,15 +2,16 @@
 
 katz_deli = []
 
-def line(lineplace)
-  line_array = []
-  if lineplace.length == 0
+def line_method(numinline)
+  line_method_array = []
+  if numinline.length == 0
     puts "The line is currently empty."
   else
-    line_array.each.with_index(0) do |name, index|
-    line_array.push("#{index}. #{name}")
-  end
-  puts "The line is: #{line_array.join(" ")}"
+    line_method_array.each.with_index(0) do |name, index|
+      line_method_array.push("#{index}. #{name}")
+      # push method will push the name into the index 
+    end
+    puts "The line is: #{line_method_array.join(" ")}"
   end
 end
 
