49dc7013b4ed1e43027231072735ff5613ac4b0d
Brandon Marrero <brandonmar97@gmail.com>
2020-03-10 20:36:30 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,7 +5,7 @@ def line(lineplace)
   if lineplace.length == 0
     puts "The line is currently empty."
   else
-    line_array.each_with_index
+    line_array.each_with_index(0) 
     puts "The line is currently: 1. a"
   
   end
