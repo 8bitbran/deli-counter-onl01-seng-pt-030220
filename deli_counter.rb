e72d74eaa20420faacce084e6aafd727daf52bc5
Brandon Marrero <brandonmar97@gmail.com>
2020-03-10 21:03:13 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,16 +2,15 @@
 
 katz_deli = []
 
-def line_method(numinline)
-  line_method_array = []
+def line(numinline)
+  line_array = []
   if numinline.length == 0
     puts "The line is currently empty."
   else
-    line_method_array.each.with_index(0) do |name, index|
-      line_method_array.push("#{index}. #{name}")
-      # push method will push the name into the index 
+    line_array.each.with_index(0) do |name, index|
+      line_array.push("#{index}. #{name}")
     end
-    puts "The line is: #{line_method_array.join(" ")}"
+    puts "The line is: #{line_array.join(" ")}"
   end
 end
 
