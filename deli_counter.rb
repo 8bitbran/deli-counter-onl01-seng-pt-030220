6f1f515f8aa1b64d983570d4ce239cc594e31f36
Brandon Marrero <brandonmar97@gmail.com>
2020-03-10 20:34:15 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,18 @@
 # Write your code here.
+
+def line(katz_deli)
+  
+  if katz_deli.size == 0
+    puts "The line is currently empty."
+  else
+    puts "The line is currently: 1. a"
+  
+  end
+end
+
+def take_a_number(array, string)
+  line_place = []
+  array.each_with_index do |place|
+    
+  end
+end
